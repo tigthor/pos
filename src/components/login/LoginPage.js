06b540fb4e194897e57b4ad2fb53e99eaaf02c5c
tigthor <tigthor1337@gmail.com>
2021-04-20 08:44:34 -0500

    Final Preview

--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -86,7 +86,7 @@ class LoginPage extends Component {
           <Paper className={classes.paper}>
             <form onSubmit={this.onSubmit}>
               <div>
-                <span>Welcome to Easy POS</span>
+                <span>Dobrodošli u POS</span>
               </div>
 
               <ErrorMessage
